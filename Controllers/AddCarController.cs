0236f6a80127a35f1890857540b4f0a3e4f31105
Venu Kuruganti <venu.kuruganti@gmail.com>
2021-12-10 11:11:41 +0530

    Added form to add car
    Adding a new car form added and also updated CSS styles.

---
+++ Controllers/AddCarController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using SimpleMVCApplication.Models;
+
+namespace SimpleMVCApplication.Controllers
+{
+    public class AddCarController : Controller
+    {
+        // GET: AddCar
+        public ActionResult GetForm()
+        {
+            SelectList CarTypes = new SelectList(
+                new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "--Please Select Car Type--", Value = "-1"},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.CarType), Car.CarType.Convertible), Value = ((int)Car.CarType.Convertible).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.CarType), Car.CarType.Coupe), Value = ((int)Car.CarType.Coupe).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.CarType), Car.CarType.Roadster), Value = ((int)Car.CarType.Roadster).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.CarType), Car.CarType.Sedan), Value = ((int)Car.CarType.Sedan).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.CarType), Car.CarType.SUV), Value = ((int)Car.CarType.SUV).ToString()}
+                }, "Value", "Text", "-1");
+
+            SelectList TransmissionTypes = new SelectList(
+                new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "--Please Select Transmission Type--", Value = "-1"},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.Transmission), Car.Transmission.Automatic), Value = ((int)Car.Transmission.Automatic).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.Transmission), Car.Transmission.Manual), Value = ((int)Car.Transmission.Manual).ToString()},
+                    new SelectListItem { Text = Enum.GetName(typeof(Car.Transmission), Car.Transmission.Tiptronic), Value = ((int)Car.Transmission.Tiptronic).ToString()}
+                }, "Value", "Text", "-1");            
+
+            ViewBag.CarTypes = CarTypes;
+            ViewBag.TransmissionTypes = TransmissionTypes;
+
+            return View("AddNewCar");
+        }
+
+        [HttpPost]
+        public ActionResult GetForm(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                //Save model to XML File
+                string Path = Server.MapPath("../XML/Cars.xml");
+
+                car.SaveNewCar(Path);
+
+                return View(new Car());
+            }
+            else
+            {
+                return View(car);
+            }
+            
+        }
+    }
+}
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace SimpleMVCApplication.Models
 {
@@ -12,26 +14,43 @@ namespace SimpleMVCApplication.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [Display(Name = "Brand")]
         public string Brand { get; set; }
-
+        
+        [Required]
+        [Display(Name ="Model")]
         public string Model { get; set; }
 
+        [Required(ErrorMessage = "Car type is required")]
+        [Display(Name ="Type of Car")]
         public CarType TypeofCar { get; set; }
 
+        [Required(ErrorMessage = "Transmission type is required")]
+        [Display(Name ="Transmission Type")]
         public Transmission TransmissionType { get; set; }
 
+        [Display(Name ="Engine HP")]
         public int HorsePower { get; set; }
 
+        [Display(Name ="Maximum Speed")]
         public int MaximumSpeed { get; set; }
 
+        [Display(Name ="Economy /  MPG")]
         public string Mileage { get; set; }
 
+        [Required]
+        [Display(Name ="Colour")]
         public string Colour { get; set; }
-
+                
+        [Display(Name ="Car Description")]
         public string Description { get; set; }
 
+        [Required]
+        [Display(Name ="Price")]
+        [Range(10000, 1000000)]
         public double Price { get; set; }
-
+        
         public string ImageURL { get; set; }
 
         public Car() {  /* Default constructor */  }
@@ -52,6 +71,86 @@ namespace SimpleMVCApplication.Models
             Tiptronic
         }
 
+        public bool SaveNewCar(string Path)
+        {
+            bool success = true;
 
+            try
+            {
+
+
+                XmlDocument XDoc = new XmlDocument();
+                XDoc.LoadXml(Path);
+
+                XmlNode node = XDoc.CreateNode(XmlNodeType.Element, "Car", null);
+
+                XmlNode child = XDoc.CreateNode(XmlNodeType.Element, "Id", null);
+                child.InnerText = Id.ToString();
+                node.AppendChild(child);
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Brand", null);
+                child.InnerText = Brand;
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Model", null);
+                child.InnerText = Model;
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Colour", null);
+                child.InnerText = Colour;
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "TypeofCar", null);
+                child.InnerText = TypeofCar.ToString();
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Transmission", null);
+                child.InnerText = TransmissionType.ToString();
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Horsepower", null);
+                child.InnerText = HorsePower.ToString();
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "MaxmimumSpeed", null);
+                child.InnerText = MaximumSpeed.ToString();
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Mileage", null);
+                child.InnerText = Mileage;
+                node.AppendChild(child);
+
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Price", null);
+                child.InnerText = Price.ToString();
+                node.AppendChild(child);
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "Description", null);
+                child.InnerText = Description;
+                node.AppendChild(child);
+
+                child = XDoc.CreateNode(XmlNodeType.Element, "ImageURL", null);
+                child.InnerText = ImageURL;
+                node.AppendChild(child);
+
+                XDoc.DocumentElement.AppendChild(node);
+
+                XDoc.Save(Path);
+
+                return success;
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                throw ex;
+            }
+        }
     }
 }
